e835597918217437cc082c931cae250cc17d4ce9
Seokhyun0079 <seokhyun0324@gmail.com>
2022-06-12 22:58:07 +0900

    fix string to array

--- back/myFirstDjango/views.py
+++ back/myFirstDjango/views.py
@@ -4,12 +4,14 @@ from django.http import JsonResponse
 from .models import Employee
 from django.utils import timezone
 from django.core import serializers
+import json
 
 
 def index(request):
-    empList = serializers.serialize("json", Employee.objects.filter(
+
+    empList = json.loads(serializers.serialize("json", Employee.objects.filter(
         pub_date__lte=timezone.now()
-    ))
+    )))
     res = {
         'data': empList
     }
