ae5414bb0772a3ecfb9b8cf4b0d2060c0b2ae1a1
Seokhyun0079 <seokhyun0324@gmail.com>
2022-06-16 21:07:46 +0900

    add indexPage

--- front/src/pages/emp/index.js
+++ front/src/pages/emp/index.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 const IndexPage = () => {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [emps, setEmps] = useState([]);
     React.useEffect(() => {
+        if (isLoaded) {
+            return
+        }
+        setIsLoaded(true)
         axios.get('/api/myFirstDjango/').then((response) => {
-            console.dir(response)
-        });
+            console.dir(response.data.data)
+            setEmps(response.data.data)
+            setIsLoaded(false)
+        })
     }, []);
-    return (<div>test</div>)
-
+    if (isLoaded) {
+        return <>now loading...</>
+    }
+    return (
+        <>
+            {(
+                !isLoaded && Array.isArray(emps) &&
+                < div >
+                    {
+                        emps.map((emp) => (
+                            <div>{emp.fields.employee_name}</div>
+                        ))
+                    }
+                </div>
+            )
+            }
+        </>
+    )
 }
 
 export default IndexPage
